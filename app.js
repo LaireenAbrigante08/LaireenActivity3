9219162332709da2947db43cad3efb16c7a36560
LaireenAbrigante08 <Laireenabrigante02@gmail.com>
2024-09-20 22:38:46 +0800

    Initial commit

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const routes = require('./routes/routes');
+//const views = require('./views/views');
+const app = express();
+app.set('view engine', 'ejs');
+
+app.use('/' , routes);
+
+
+app.listen(3000, ()=>{
+    console.log('server running on http://localhost:3000');
+});
